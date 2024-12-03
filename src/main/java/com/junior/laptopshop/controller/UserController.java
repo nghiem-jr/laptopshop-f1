d80d67f13e11c3d3c29af9c6776c181c623a6562
nghiem-jr <doduongnghiem03@gmail.com>
2024-12-03 11:02:00 +0700

    set up file create.html de co the truyen du lieu tu view->controller,tao giao dien sign up

--- src/main/java/com/junior/laptopshop/controller/UserController.java
+++ src/main/java/com/junior/laptopshop/controller/UserController.java
@@ -3,11 +3,14 @@ package com.junior.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.junior.laptopshop.domain.User;
 import com.junior.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -24,18 +27,16 @@ public class UserController {
         model.addAttribute("variable", value);
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute User junior) {
+        System.out.println("run here" + junior);
+        return "hello";
+    }
 }
-// @RestController
-// public class UserController {
-
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-// }
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -10,13 +10,14 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="/css/demo.css">
+    <!-- <link rel="stylesheet" href="/css/demo.css"> -->
 </head>
 
 <body>
     hello from jsp
     <h1>${variable}</h1>
     <button class="btn btn-success">Submit</button>
+    <button class="btn btn-primary">submit</button>
 </body>
 
 </html>
