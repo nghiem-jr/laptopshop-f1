3df9d5d0a1dec52f24c9307bd4c48d7335345bda
nghiem-jr <doduongnghiem03@gmail.com>
2024-12-12 10:08:27 +0700

    hoan thanh chuc nang update thong tin user

--- src/main/java/com/junior/laptopshop/controller/UserController.java
+++ src/main/java/com/junior/laptopshop/controller/UserController.java
@@ -14,6 +14,7 @@ import com.junior.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -63,6 +64,19 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute User junior) {
         this.userService.handleSaveUser(junior);
+        return "redirect:/admin/user";
+    }
+
+    @PostMapping("/admin/user/update")
+    public String postUpdateUser(Model model, @ModelAttribute("newUser") User hoidanit) {
+        User currentUser = this.userService.getUserById(hoidanit.getId());
+        if (currentUser != null) {
+            currentUser.setFullName(hoidanit.getFullName());
+            currentUser.setPhone(hoidanit.getPhone());
+            currentUser.setAddress(hoidanit.getAddress());
+
+            this.userService.handleSaveUser(currentUser);
+        }
         return "redirect:/admin/user";
     }
 }
