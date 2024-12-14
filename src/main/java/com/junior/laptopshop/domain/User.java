6e0bbebe3b55583a222da1d1491c3d78351c363c
nghiem-jr <doduongnghiem03@gmail.com>
2024-12-14 12:12:33 +0700

    completed object relational mapping

--- src/main/java/com/junior/laptopshop/domain/User.java
+++ src/main/java/com/junior/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package com.junior.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +25,14 @@ public class User {
 
     private String avatar;
     // roleId
+
+    // User-many-> one-role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
     public long getId() {
         return id;
